4f0e7037a2225a6d2eb43b6c31603368292db134
Sergey Postnikov <ser.post2017@gmail.com>
2023-03-10 22:53:33 +0300

    post_photo, add_photo_to_albom implemented

--- main.py
+++ main.py
@@ -64,6 +64,31 @@ def get_wall_upload_server(group_id, access_token):
     return response.json()
 
 
+def post_photo(url, path_to_image):
+    with open(path_to_image, 'rb') as file:
+        files = {
+            'photo': file,  
+        }
+        response = requests.post(url, files=files)
+        response.raise_for_status()
+        return response.json()
+
+
+def add_photo_to_albom(group_id, photo, server, hash_photo, access_token):
+    params = {
+        'group_id': group_id,
+        'photo': photo,
+        'server': server,
+        'hash': hash_photo,
+        'access_token': access_token,
+        'v': 5.131,
+    }
+    url = 'https://api.vk.com/method/photos.saveWallPhoto'
+    response = requests.post(url=url, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
 if __name__ == '__main__':
     # url = get_img_url(353)
     # save_picture(url=url, directory='images')
@@ -71,8 +96,11 @@ if __name__ == '__main__':
 
     load_dotenv()
     access_token = os.environ['VK_BOT_ACCESS_KEY']
-    # user_id = os.environ['VK_USER_ID']
-    # pprint(get_groups(user_id=user_id, access_token=access_token))
+    user_id = os.environ['VK_USER_ID']
 
     group_id = os.environ['VK_XKCD_GROUP_ID']
-    pprint(get_wall_upload_server(group_id, access_token=access_token))
+    url = get_wall_upload_server(group_id, access_token=access_token).get('response').get('upload_url')
+    resp = post_photo(url=url, path_to_image='./images/35e508cb6cpython.png')
+    server, photo, hash_photo = map(lambda key: resp[key], resp)
+    resp = add_photo_to_albom(group_id, photo, server, hash_photo, access_token)
+    pprint(resp)
