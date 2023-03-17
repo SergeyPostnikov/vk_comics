94c87aa1d805aa015835c202a5052e1f669d6a3f
Sergey Postnikov <ser.post2017@gmail.com>
2023-03-18 01:18:17 +0300

    implemented "post random comics"

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from os.path import join
 from urllib.parse import urlparse, unquote
 from hashlib import blake2b
 from pathlib import Path
-
-from pprint import pprint
 from dotenv import load_dotenv
+
+from random import randint
+import os
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -24,33 +25,37 @@ def save_picture(url, directory, params=None):
     response = requests.get(url, params=params)
     response.raise_for_status()
     os.makedirs(directory, exist_ok=True)
+    filename = get_filename(url)
+    path_to_image = join(BASE_DIR, 'images', filename)
 
-    with open(join(BASE_DIR, 'images', get_filename(url)), 'wb') as file:
+    with open(path_to_image, 'wb') as file:
         file.write(response.content)
 
+    return path_to_image
 
-def get_img_url(num):
+
+def get_comics_amount():
+    url = 'https://xkcd.com/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()['num']
+
+
+def get_comics(num):
     url = f'https://xkcd.com/{num}/info.0.json'
     response = requests.get(url)
-    return response.json().get('img')
+    response.raise_for_status()
+    comics = response.json()
+    url = comics['img']
+    alt = comics['alt']
+    return url, alt
 
 
-def get_comment(num):   
-    url = f'https://xkcd.com/{num}/info.0.json'
-    response = requests.get(url)
-    return response.json().get('alt')
-
-
-def get_groups(user_id, access_token):
-    url = 'https://api.vk.com/method/groups.get'
-    headers = {}
-    params = {
-        'user_id': user_id,
-        'access_token': access_token,
-        'v': 5.131
-    }
-    response = requests.get(url, params=params, headers=headers)
-    return response.json()
+def get_random_comics(comics_amount):
+    comics_number = randint(1, comics_amount)
+    url, alt = get_comics(num=comics_number)
+    path_to_image = save_picture(url=url, directory='images')
+    return path_to_image, alt
 
 
 def get_wall_upload_server(group_id, access_token):
@@ -61,6 +66,7 @@ def get_wall_upload_server(group_id, access_token):
         'v': 5.131
     }
     response = requests.get(url, params=params)
+    response.raise_for_status()
     return response.json()['response']['upload_url']
 
 
@@ -107,12 +113,11 @@ def post_photo_on_wall(group_id, owner_id, massage, photo_id, access_token):
     return response.json()
 
 
-def post_comix_on_wall(path_to_image, access_token):
+def post_comics_on_wall(path_to_image, alt, access_token):
     url = get_wall_upload_server(group_id, access_token=access_token)
     server, photo, hash_photo = post_photo_on_server(url=url, path_to_image=path_to_image)
     resp = add_photo_to_albom(
         group_id=group_id, 
-        # user_id=user_id, 
         photo=photo, 
         server=server, 
         hash_photo=hash_photo, 
@@ -125,21 +130,27 @@ def post_comix_on_wall(path_to_image, access_token):
         group_id=group_id, 
         photo_id=photo_id,
         owner_id=owner_id, 
-        massage='bla-bla', 
+        massage=alt, 
         access_token=access_token
         )
+
+
+def main(access_token, group_id):
+    comics_amount = get_comics_amount()
+    path_to_image, alt = get_random_comics(comics_amount)
+
+    post_comics_on_wall(
+        path_to_image=path_to_image,
+        alt=alt,
+        access_token=access_token
+        )
+    
+    os.remove(path_to_image)
 
 
 if __name__ == '__main__':
-    # url = get_img_url(353)
-    # save_picture(url=url, directory='images')
-    # print(get_comment(353))
-
     load_dotenv()
     access_token = os.environ['VK_ACCESS_KEY']
-    user_id = os.environ['VK_USER_ID']
     group_id = os.environ['VK_XKCD_GROUP_ID']
-    post_comix_on_wall(
-        path_to_image='./images/35e508cb6cpython.png',
-        access_token=access_token
-        )
+
+    main(access_token=access_token, group_id=group_id)
